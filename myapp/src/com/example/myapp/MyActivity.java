cec69150b7fe7ebb0dae66fa0de37f75682290f1
testtwo <zhang_guo_j0571@sina.com>
2014-07-23 14:57:37 +0800

    含有中文

--- myapp/src/com/example/myapp/MyActivity.java
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -12,5 +12,10 @@ public class MyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 //        ttt
+        cd F:\\githubHzbema\\hello
+        Git Repository服务器，采用的ssh协议的话，利用生成的key，建立SSH信任通道，Git库同步操作也非常简单。
+        在服务器上将Git Server搭建完毕啦，其实也没有什么搭建的，只要在服务器上安装一下Git，配置一下SSH，步骤还是非常简单的。以后的一些个人项目会逐渐切换到Git上，但是在公司开发项目，目前还是Subversion为主，Git和SVN并行吧，Git的优点还是很多的。
+        Git 是一款非常优秀的代码版本控制系统，它可以有效地实现多人异地办公，此外，你可以提交代码到本地仓库中，然后一次性提交到远程仓库。
+       dd
     }
 }
